95cf2f70c717a26312f5f6995a2cee02a93e1f35
Dom Raymond <dom@notdom.com>
2024-05-20 10:31:20 -0400

    style tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -4,73 +4,13 @@ import validator from '@rjsf/validator-ajv8';
 import { Button, Spin, Menu, Flex, Layout, Card, Table, Badge, Drawer, Space, InputNumber } from 'antd';
 const { Column } = Table;
 const { Header, Sider, Content } = Layout;
+
+import {headerStyle, contentStyle, siderStyle,layoutStyle, providers_list} from './constants.js';
+
 import compSchema from  './forms/company_form.json';
 import indSchema from  './forms/ind_form.json';
 import empSchema from  './forms/emp_form.json';
 
-
-
-
-const headerStyle = {
-  textAlign: 'center',
-  fontSize:'18px',
-  fontWeight: '600',
-  color: '#fff',
-  height: 64,
-  paddingInline: 48,
-  lineHeight: '64px',
-  backgroundColor: '#4096ff',
-};
-const contentStyle = {
-  minHeight: 120,
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#0958d9',
-};
-const siderStyle = {
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#1677ff',
-};
-const layoutStyle = {
-  borderRadius: 8,
-  overflow: 'hidden',
-  width: '90%',
-  maxWidth: 'calc(100% - 8px)',
-};
-
-const providers_list = [
-  {label: 'Select Provider', key: '1'},
-  {label: 'ADP Run', key: 'adp_run'},
-  {label: 'Bamboo HR', key: 'bamboo_hr'}, 
-  {label: 'Bamboo HR (API)', key: 'bamboo_hr_api'},
-  {label: 'HiBob', key: 'bob'},
-  {label: 'Gusto', key: 'gusto'},
-  {label: 'Humaans', key: 'humaans'},
-  {label: 'Insperity', key: 'insperity'},
-  {label: 'Justworks', key: 'justworks'},
-  {label: 'Namely', key: 'namely'}, 
-  {label: 'Paychex Flex', key: 'paychex_flex'}, 
-  {label: 'Paychex Flex (API)', key: 'paychex_flex_api'}, 
-  {label: 'Paycom', key: 'paycom'}, 
-  {label: 'Paycom (API)', key: 'paycom_api'}, 
-  {label: 'Paylocity', key: 'paylocity'}, 
-  {label: 'Paylocity (API)', key: 'paylocity_api'}, 
-  {label: 'Personio', key: 'personio'}, 
-  {label: 'Quickbooks', key: 'quickbooks'}, 
-  {label: 'Rippling', key: 'rippling'}, 
-  {label: 'Sage HR', key: 'sage_hr'}, 
-  {label: 'Sapling', key: 'sapling'}, 
-  {label: 'Squoia One', key: 'sequoia_one'}, 
-  {label: 'Square Payroll', key: 'square_payroll'}, 
-  {label: 'Trinet', key: 'trinet'}, 
-  {label: 'Trinet (API)', key: 'trinet_api'}, 
-  {label: 'Ulti Pro', key: 'ulti_pro'},
-  {label: 'Wave', key: 'wave'},
-  {label: 'Workday', key: 'workday'},
-  {label: 'Zenefits', key: 'zenefits'},
-  {label: 'Zenefits (API)', key: 'zenefits_api'}
-];
 
 
 const App = () => {
@@ -80,15 +20,12 @@ const App = () => {
   const [compName, setCompName] = useState("");
   const [drawerOpen, setDrawerOpen] = useState(false);
   const [individual, setIndividual] = useState([]);
-  const [employeeCount, setEmployeeCount] = useState("");
+  const [employeeCount, setEmployeeCount] = useState("10");
 
   const onClose = () => {
-    setDrawerOpen(false);
   };
   
   const onSelectProvider = (e) => {
-    //console.log('click ', e);
-
     fetch("/api/sandbox/create", {
       method: "POST", // *GET, POST, PUT, DELETE, etc.
       headers: {"Content-Type": "application/json"},
@@ -100,7 +37,6 @@ const App = () => {
     }).then(function(response){
       return response.json();
     }).then(function(data){
-      //console.log(data);
       updateForms(data);
     })
     .catch(function(error) {
@@ -184,9 +120,9 @@ const App = () => {
        
       <Layout style={layoutStyle}>
         <Sider width="20%" style={siderStyle}>
-          <Space direction='horizontal'>
-              Number of Employees
-          <InputNumber min={1} max={200} defaultValue={10} onChange={(e)=>{setEmployeeCount(e)}} />
+          <Space direction='horizontal' >
+              <Space style={{paddingLeft: '6px', fontWeight:500}}>Directory Size</Space>
+          <InputNumber min={1} max={200} defaultValue={10} size="large" variant="filled" onChange={(e)=>{setEmployeeCount(e)}} />
           </Space>
           <Menu
             onClick={onSelectProvider}
@@ -283,7 +219,7 @@ const App = () => {
             open={drawerOpen}
             extra={
               <Space>
-                <Button onClick={onClose}>Close</Button>
+                <Button onClick={()=>{setDrawerOpen(false)}}>Close</Button>
               </Space>
             }
           >
---
+++ src/constants.js
@@ -0,0 +1,63 @@
+//Layout Constants
+export const headerStyle = {
+    textAlign: 'center',
+    fontSize:'18px',
+    fontWeight: '600',
+    color: '#fff',
+    height: 64,
+    paddingInline: 48,
+    lineHeight: '64px',
+    backgroundColor: '#4096ff',
+  };
+export const contentStyle = {
+    minHeight: 120,
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#0958d9',
+  };
+export const siderStyle = {
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#1677ff',
+  };
+export  const layoutStyle = {
+    borderRadius: 8,
+    overflow: 'hidden',
+    width: '80%',
+    maxWidth: '1000px',
+    margin: 'auto'
+  };
+
+//List of Provicers  
+export const providers_list = [
+    {label: 'Select Provider', key: '1'},
+    {label: 'ADP Run', key: 'adp_run'},
+    {label: 'Bamboo HR', key: 'bamboo_hr'}, 
+    {label: 'Bamboo HR (API)', key: 'bamboo_hr_api'},
+    {label: 'HiBob', key: 'bob'},
+    {label: 'Gusto', key: 'gusto'},
+    {label: 'Humaans', key: 'humaans'},
+    {label: 'Insperity', key: 'insperity'},
+    {label: 'Justworks', key: 'justworks'},
+    {label: 'Namely', key: 'namely'}, 
+    {label: 'Paychex Flex', key: 'paychex_flex'}, 
+    {label: 'Paychex Flex (API)', key: 'paychex_flex_api'}, 
+    {label: 'Paycom', key: 'paycom'}, 
+    {label: 'Paycom (API)', key: 'paycom_api'}, 
+    {label: 'Paylocity', key: 'paylocity'}, 
+    {label: 'Paylocity (API)', key: 'paylocity_api'}, 
+    {label: 'Personio', key: 'personio'}, 
+    {label: 'Quickbooks', key: 'quickbooks'}, 
+    {label: 'Rippling', key: 'rippling'}, 
+    {label: 'Sage HR', key: 'sage_hr'}, 
+    {label: 'Sapling', key: 'sapling'}, 
+    {label: 'Squoia One', key: 'sequoia_one'}, 
+    {label: 'Square Payroll', key: 'square_payroll'}, 
+    {label: 'Trinet', key: 'trinet'}, 
+    {label: 'Trinet (API)', key: 'trinet_api'}, 
+    {label: 'Ulti Pro', key: 'ulti_pro'},
+    {label: 'Wave', key: 'wave'},
+    {label: 'Workday', key: 'workday'},
+    {label: 'Zenefits', key: 'zenefits'},
+    {label: 'Zenefits (API)', key: 'zenefits_api'}
+  ];
